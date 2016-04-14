d6cb812bdab48757b0ead6ed71b16bd45324cbe2
Zhihao Yang <yang_zhihao@live.com>
2016-04-14 21:47:30 +0200

    creation of sign in & sign up html

--- IPWD-angular/app/js/cocktailService.js
+++ IPWD-angular/app/js/cocktailService.js
@@ -1,7 +1,8 @@
 yocktailApp.factory('Cocktail',function ($resource) {
   
-  this.CocktailSearch = $resource('http://addb.absolutdrinks.com/drinks/',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
+  var user;
 
+  this.CocktailSearch = $resource('http://addb.absolutdrinks.com/drinks/',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
 
   return this;
