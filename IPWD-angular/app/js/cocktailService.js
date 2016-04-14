74799f667361f0c64bd59e72aaede3da224ec5bc
Zhihao Yang <yang_zhihao@live.com>
2016-04-14 23:15:05 +0200

    Implement Sign In and Sign Up using Angular and Firebase
    Here are the related tutorials and articles:
    http://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using
    -angularjs-and-firebase-part-2--cms-22734
    http://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using
    -angularjs-and-firebase-part-3--cms-22737
    https://www.firebase.com/docs/web/libraries/angular/api.html

--- IPWD-angular/app/js/cocktailService.js
+++ IPWD-angular/app/js/cocktailService.js
@@ -2,6 +2,8 @@ yocktailApp.factory('Cocktail',function ($resource) {
   
   var user;
 
+  var firebaseObj = new Firebase("https://yocktail.firebaseio.com");
+
   this.CocktailSearch = $resource('http://addb.absolutdrinks.com/drinks/',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
 
--- IPWD-angular/app/js/signupCtrl.js
+++ IPWD-angular/app/js/signupCtrl.js
@@ -1,4 +1,25 @@
-yocktailApp.controller('SignupCtrl', function ($scope,Cocktail) {
+yocktailApp.controller('SignupCtrl', function ($scope, $firebaseAuth, Cocktail) {
 
+    var firebaseObj = new Firebase("https://yocktail.firebaseio.com");
+    var authObj = $firebaseAuth(firebaseObj);
+
+    $scope.SignUp = function(){
+
+   		if (!$scope.regForm.$invalid) {
+	        var email = $scope.user.email;
+            var password = $scope.user.password;
+
+            if (email && password) {
+                authObj.$createUser({ email: $scope.user.email, password: $scope.user.password })
+                    .then(function() {
+                        // do things if success
+                        console.log('User creation success');
+                    }, function(error) {
+                        // do things if failure
+                        console.log(error);
+                    });
+            }
+	    }
+    };
 
 });
