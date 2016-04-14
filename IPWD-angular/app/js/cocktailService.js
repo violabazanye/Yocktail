2790a5f31a46981d424494c86dcde235cafb792a
Viola Bazanye <violabazanye@Violas-MBP.lan>
2016-04-14 23:08:46 +0200

    search halfway complete

--- IPWD-angular/app/js/cocktailService.js
+++ IPWD-angular/app/js/cocktailService.js
@@ -2,7 +2,9 @@ yocktailApp.factory('Cocktail',function ($resource) {
   
   this.CocktailSearch = $resource('http://addb.absolutdrinks.com/drinks/',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
+  this.CocktailQuerySearch = $resource('http://addb.absolutdrinks.com/drinks/with/:input',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
+  this.PopularCocktails = $resource('http://addb.absolutdrinks.com/drinks/rating/:numerical_condition',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
   return this;
 
--- IPWD-angular/app/js/exploreCtrl.js
+++ IPWD-angular/app/js/exploreCtrl.js
@@ -5,7 +5,7 @@ yocktailApp.controller('ExploreCtrl', function ($scope,Cocktail) {
 	      $scope.search($cookieStore.get("query"));
 	    }else{*/
 	      $scope.status = "Searching...";
-	      Cocktail.CocktailSearch.get({callback:"JSON_CALLBACK"},function(data){
+	      Cocktail.CocktailSearch.get(function(data){
 	        $scope.cocktails=data.result;
 	        $scope.status = "Showing " + data.result.length + " results";
 	      },function(data){
@@ -13,5 +13,16 @@ yocktailApp.controller('ExploreCtrl', function ($scope,Cocktail) {
 	      });
 	   // }
 	});
+
+	$scope.search = function(query) {
+	    //$cookieStore.put("query", query);
+	  	$scope.status = "Searching...";
+	   	Cocktail.CocktailQuerySearch.get({input:query},function(data){
+	    	$scope.cocktails=data.result;
+	     	$scope.status = "Showing " + data.result.length + " results";
+	   	},function(data){
+	     	$scope.status = "There was an error. Try again.";
+	   	});
+	}
 
 });
