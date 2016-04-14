1a55eaa70a774fedc666e500575b7b63cd92b1b2
Viola Bazanye <violabazanye@Violas-MBP.lan>
2016-04-14 16:45:52 +0200

    cannot access api

--- IPWD-angular/app/js/cocktailService.js
+++ IPWD-angular/app/js/cocktailService.js
@@ -1,36 +1,9 @@
-// Here we create an Angular service that we will use for our 
-// model. In your controllers (or other services) you can include the
-// dependency on any service you need. Angular will insure that the
-// service is created first time it is needed and then just reuse it
-// the next time.
 yocktailApp.factory('Cocktail',function ($resource) {
   
-  var numberOfGuest = 2;
-
-
-  this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
-  }
-
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
-
-
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
+  this.CocktailSearch = $resource('http://addb.absolutdrinks.com/drinks/',{apiKey:'003234e57e7a4a0a83f3b1d671c597da'});
 
 
 
-
-
-  // Angular service needs to return an object that has all the
-  // methods created in it. You can consider that this is instead
-  // of calling var model = new DinnerModel() we did in the previous labs
-  // This is because Angular takes care of creating it when needed.
   return this;
 
 });
--- IPWD-angular/app/js/exploreCtrl.js
+++ IPWD-angular/app/js/exploreCtrl.js
@@ -1,4 +1,17 @@
 yocktailApp.controller('ExploreCtrl', function ($scope,Cocktail) {
 
+	$scope.$on('$viewContentLoaded', function(){
+	    /*if($cookieStore.get("query") != undefined){
+	      $scope.search($cookieStore.get("query"));
+	    }else{*/
+	      $scope.status = "Searching...";
+	      Cocktail.CocktailSearch.get({callback:"JSON_CALLBACK"},function(data){
+	        $scope.cocktails=data.result;
+	        $scope.status = "Showing " + data.result.length + " results";
+	      },function(data){
+	        $scope.status = "There was an error. Try again.";
+	      });
+	   // }
+	});
 
 });
