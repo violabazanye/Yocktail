70cec1c909679ed40ad192405c9f4c8d6153e067
Viola Bazanye <violabazanye@Violas-MBP.lan>
2016-04-15 14:13:22 +0200

    edited home page

--- IPWD-angular/app/js/homeCtrl.js
+++ IPWD-angular/app/js/homeCtrl.js
@@ -2,12 +2,16 @@ yocktailApp.controller('HomeCtrl', function ($scope,Cocktail) {
 
 	$scope.$on('$viewContentLoaded', function(){
 
-	      Cocktail.PopularCocktails.get({numerical_condition:"gt50"},function(data){
+	      Cocktail.PopularCocktails.get({numerical_condition:"gt90"},function(data){
 	        $scope.cocktails=data.result;
 	      },function(data){
 	        $scope.status = "There was an error. Try again.";
 	      });
 	   
 	});
+
+	$scope.go = function ( path ) {
+  		$location.path( path );
+	};
 
 });
