16358d04ea18406f0c8a9d16ec8894014e145b5c
Zhihao Yang <yang_zhihao@live.com>
2016-04-16 21:24:08 +0200

    revise the code of signin and signup

--- IPWD-angular/app/js/signinCtrl.js
+++ IPWD-angular/app/js/signinCtrl.js
@@ -14,6 +14,8 @@ yocktailApp.controller('SigninCtrl', function ($scope, $firebaseAuth, $location,
 	        })
 	        .then(function(user) {
 	            //Success callback
+	            console.log("user");
+	            console.log(user);
 	            console.log('SigninCtrl Authentication successful');
 	            Cocktail.setUser(username);
 				console.log("user:" + Cocktail.getUser());
@@ -21,6 +23,8 @@ yocktailApp.controller('SigninCtrl', function ($scope, $firebaseAuth, $location,
 	            $location.path('/profile');
 	        }, function(error) {
 	            //Failure callback
+	            $scope.regError = true;
+				$scope.regErrorMessage = "Sorry, failed to sign in. Please try again.";
 	            console.log('SigninCtrl Authentication failure');
 	        });
 	}
--- IPWD-angular/app/js/signupCtrl.js
+++ IPWD-angular/app/js/signupCtrl.js
@@ -6,23 +6,37 @@ yocktailApp.controller('SignupCtrl', function ($scope, $firebaseAuth, $location,
     $scope.SignUp = function(){
 
    		if (!$scope.regForm.$invalid) {
+            var name = $scope.user.name;
 	        var email = $scope.user.email;
+            var birthday = $scope.user.birthday;
             var password = $scope.user.password;
+            var confirmedPassword = $scope.user.confirmedPassword;
 
-            if (email && password) {
-                authObj.$createUser({ email: $scope.user.email, password: $scope.user.password })
-                    .then(function() {
-                        // do things if success
-                        Cocktail.setUser(email);
-                        console.log('SignupCtrl User creation success');
-                        $location.path('/profile');
-                    }, function(error) {
-                        // do things if failure
-                        console.log(error);
-                        $scope.regError = true;
-						$scope.regErrorMessage = error.message;
-                        console.log('SignupCtrl signup failure with error: ' + error.message);
-                    });
+            if (name && email && birthday && password && confirmedPassword) {
+                if (password == confirmedPassword) {
+
+                    // create user in firebase
+                    authObj.$createUser({ name: name, email: email, birthday: birthday, password: password })
+                            .then(function() {
+                                    // do things if success
+                                    Cocktail.setUser(email);
+                                    console.log('SignupCtrl User creation success');
+                                    $location.path('/profile');
+                                }, function(error) {
+                                    // do things if failure
+                                    console.log(error);
+                                    $scope.regError = true;
+                                    $scope.regErrorMessage = error.message;
+                                    console.log('SignupCtrl signup failure with error: ' + error.message);
+                                }
+                            );
+                }else{
+                    $scope.regError = true;
+                    $scope.regErrorMessage = "The two passwords does not match.";
+                }
+            }else{
+                $scope.regError = true;
+                $scope.regErrorMessage = "Please fill out all the required information.";
             }
 	    }
     };
