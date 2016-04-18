e462c9d7ff2db6d2572374f7bad92d22e0f2d8fe
Zhihao Yang <yang_zhihao@live.com>
2016-04-18 10:15:55 +0200

    bug fixed in profileCtrl

--- IPWD-angular/app/js/profileCtrl.js
+++ IPWD-angular/app/js/profileCtrl.js
@@ -6,10 +6,39 @@ yocktailApp.controller('ProfileCtrl', function ($scope, $firebaseAuth, $location
 
 	// creating a Firebase database Reference for users
     var usersRef = new Firebase("https://yocktail.firebaseio.com/web/data/users");  
-
     // check if visitingUid is in routeParams
 	if (visitingUid) {
 		// visitingUid specified
+		// check if it is the profile page of the signed in user
+		if(currentUser == ''){
+			// no signed in user
+			// do nothing
+	    }else{
+	    	// user signed in
+	    	if(currentUser.uid == visitingUid){
+	    		isSameUser = true;
+	    	}else{
+	    		// do nothing
+	    	}
+	    }
+
+	    if (isSameUser) {
+	    	// is the profile page of the signed in user 
+	    	$scope.user = currentUser;
+	    }else{
+	    	// not the profile page of the signed in user 
+	    	// get data of the profile page of the user with visitingUid
+	    	usersRef.child(visitingUid).once("value", function(data){
+	        	console.log("ProfileCtrl visiting user data");
+	        	//console.log(data.val());
+	        	var visitingUserData = data.val();
+	            var visitingUser = { uid: visitingUid, name: visitingUserData.name};
+				console.log("visitingUser: " + visitingUser);
+
+	            $scope.user = visitingUser;
+	            $scope.$apply();
+		    });
+    	}
 	}else{
 		// no visitingUid specified
 		// check if user signed in
@@ -23,36 +52,5 @@ yocktailApp.controller('ProfileCtrl', function ($scope, $firebaseAuth, $location
 			$location.path('/profile/'+currentUser.uid);
 		}
 	}
-
-	// check if it is the profile page of the signed in user
-	if(currentUser == ''){
-		// no signed in user
-		// do nothing
-    }else{
-    	// user signed in
-    	if(currentUser.uid == visitingUid){
-    		isSameUser = true;
-    	}else{
-    		// do nothing
-    	}
-    }
-
-    if (isSameUser) {
-    	// is the profile page of the signed in user 
-    	$scope.user = currentUser;
-    }else{
-    	// not the profile page of the signed in user 
-    	// get data of the profile page of the user with visitingUid
-    	usersRef.child(visitingUid).once("value", function(data){
-        	console.log("ProfileCtrl visiting user data");
-        	//console.log(data.val());
-        	var visitingUserData = data.val();
-            var visitingUser = { uid: visitingUid, name: visitingUserData.name};
-			console.log("visitingUser: " + visitingUser);
-
-            $scope.user = visitingUser;
-            $scope.$apply();
-	    });
-    }
 
 });
