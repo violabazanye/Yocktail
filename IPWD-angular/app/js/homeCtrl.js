0c25a52257f159778b36703136adf956f5a35252
Zhihao Yang <yang_zhihao@live.com>
2016-04-23 01:19:49 +0200

    show user created cocktail in the home page

--- IPWD-angular/app/js/homeCtrl.js
+++ IPWD-angular/app/js/homeCtrl.js
@@ -15,4 +15,34 @@ yocktailApp.controller('HomeCtrl', function ($scope,Cocktail, $location) {
 	$scope.search = function(query){
 		$location.path("/explore/" + query);
 	}
+
+    var usersRef = new Firebase("https://yocktail.firebaseio.com/web/data/users");
+	var cocktailsRef = new Firebase("https://yocktail.firebaseio.com/web/data/cocktails"); 
+
+	$scope.userMadeCocktails = [];
+
+	cocktailsRef.limitToLast(4).once("value", function(snapshot) {
+
+		snapshot.forEach(function(data) {
+
+			var cocktail = data.val();
+			console.log("cocktail");
+			console.log(cocktail);
+
+			cocktail.key = data.key();
+
+			var creatorUid = cocktail.creator_uid;
+			console.log("creatorUid :"+creatorUid);
+
+			usersRef.child(creatorUid).once("value", function(data){
+				cocktail.creator = data.val();
+
+				console.log("cocktail.creator :");
+				console.log(cocktail.creator);
+
+				$scope.userMadeCocktails.push(cocktail);
+				$scope.$apply();
+			});
+		});
+	});
 });
